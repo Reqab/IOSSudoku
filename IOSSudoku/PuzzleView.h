625ab182b600960891555ffc335cab474b5d97bb
Adam Matthew Bennett <adambennett@ecs323-11.labs.encs>
2015-02-25 14:31:14 -0800

    added puzzle view

---
+++ IOSSudoku/PuzzleView.h
@@ -0,0 +1,13 @@
+//
+//  PuzzleView.h
+//  IOSSudoku
+//
+//  Created by Adam Matthew Bennett on 2/25/15.
+//  Copyright (c) 2015 Adam M. Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PuzzleView : UIView
+
+@end
---
+++ IOSSudoku/PuzzleView.m
@@ -0,0 +1,21 @@
+//
+//  PuzzleView.m
+//  IOSSudoku
+//
+//  Created by Adam Matthew Bennett on 2/25/15.
+//  Copyright (c) 2015 Adam M. Bennett. All rights reserved.
+//
+
+#import "PuzzleView.h"
+
+@implementation PuzzleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
